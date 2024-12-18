7e88ee283121088e02c4b74a252d98a96d5a33b5
hadar <hadargerashi2005@gmail.com>
2024-12-18 23:51:58 +0200

    second

--- config/DB.js
+++ config/DB.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 export function connectToDB()
 {
-    connect(process.env.DB_URL).then(con=>console.log("mongo DB connected"))
+    connect("mongodb://localhost:27017/StudiesDB").then(con=>console.log("mongo DB connected"))
     .catch(err=>{
         console.log("cannot connect mongo DB "+err)
         process.exit(1)
