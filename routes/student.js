0185c70e769e2ba68875757fc13b9a60bb72b77d
hadar <hadargerashi2005@gmail.com>
2025-01-08 18:08:28 +0200

    fixed

--- routes/student.js
+++ routes/student.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { add_sighIn, getById, logIn_add, getAll, update } from "../controllers/student.js";
+import { add_sighIn, getById, logIn_add, getAll, update,deleteById } from "../controllers/student.js";
 
 
 const routes=Router()
@@ -10,7 +10,7 @@ routes.get("/:id",getById)
 routes.post("/login",logIn_add)
 routes.post("/",add_sighIn)
 routes.put("/:id",update)
-routes.delete("/:id",update)
+routes.delete("/:id",deleteById)
 
 
 
